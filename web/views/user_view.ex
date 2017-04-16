40b3a64a3c389adeb800388de87f65ee1d375e60
Steven Oud <soud@protonmail.com>
2017-04-16 14:56:03 +0200

    Don't show useless user data

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -10,6 +10,6 @@ defmodule Lift.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    Map.take(user, [:id, :username, :created_at, :updated_at])
+    Map.take(user, [:id, :username])
   end
 end
