1400846b79317fe14606d2a68208a41e3259b675
Steven Oud <soud@protonmail.com>
2017-04-16 15:20:54 +0200

    Add `post.json` test

--- test/views/post_view_test.exs
+++ test/views/post_view_test.exs
@@ -18,10 +18,30 @@ defmodule Lift.PostViewTest do
   test "show.json" do
     post = insert(:post)
 
-    rendered_posts = PostView.render("show.json", post: post)
+    rendered_post = PostView.render("show.json", post: post)
 
-    assert rendered_posts == %{
+    assert rendered_post == %{
       data: PostView.render("post.json", post: post)
+    }
+  end
+
+  test "post.json" do
+    post = insert(:post)
+
+    rendered_post = PostView.render("post.json", post: post)
+
+    assert rendered_post == %{
+      id: post.id,
+      body: post.body,
+      is_locked: post.is_locked,
+      category: %{
+        id: post.category.id,
+        name: post.category.name,
+        description: post.category.description
+      },
+      user: %{ id: post.user.id, username: post.user.username },
+      created_at: post.inserted_at,
+      updated_at: post.updated_at
     }
   end
 end
