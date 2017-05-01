2550aadd6a1498ef41990a09716e90bba4157b8f
Steven Oud <soud@protonmail.com>
2017-05-01 10:53:28 +0200

    Put likes behind authentication

--- test/controllers/like_controller_test.exs
+++ test/controllers/like_controller_test.exs
@@ -1,21 +1,25 @@
 defmodule Lift.LikeControllerTest do
   use Lift.ConnCase
 
+  alias Lift.Auth
+
   setup do
     user = insert(:user)
     post = insert(:post, user: user)
+    token = Auth.generate_token(user)
+    conn = build_conn() |> put_req_header("authorization", "bearer " <> token)
 
-    {:ok, conn: build_conn(), user: user, post: post}
+    {:ok, conn: conn, user: user, post: post}
   end
 
   test "PUT /posts/:id/like likes a post", %{conn: conn} = context do
-    conn = put(conn, "/posts/#{context.post.id}/like", %{user_id: context.user.id})
+    conn = put(conn, "/posts/#{context.post.id}/like")
     assert response(conn, 204)
   end
 
   test "PUT /posts/:id/unlike unlikes a post", %{conn: conn} = context do
     like = insert(:like, user: context.user, post: context.post)
-    conn = put(conn, "/posts/#{like.post.id}/unlike", %{user_id: context.user.id})
+    conn = put(conn, "/posts/#{like.post.id}/unlike")
     assert response(conn, 204)
   end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,9 +2,9 @@ defmodule Lift.UserController do
   use Lift.Web, :controller
   use Guardian.Phoenix.Controller
 
-  plug Guardian.Plug.EnsureAuthenticated, handler: Lift.TokenController
-
   alias Lift.{Category, CategoryView}
+
+  plug Guardian.Plug.EnsureAuthenticated, handler: Lift.TokenController
 
   def show(conn, _params, user, _claims) do
     user = Repo.preload(user, [:categories])
